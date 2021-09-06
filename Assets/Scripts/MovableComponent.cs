f9c2cbacbf6ffb2ac558da3b35b55e099c60f9ba
atom5454 <atom5442@gmail.com>
2021-09-06 19:59:48 +0300

    Updated MobableComponent file name

--- Assets/Scripts/MovableComponent.cs
+++ Assets/Scripts/MovableComponent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovebleComponent : MonoBehaviour
+public class MovableComponent : MonoBehaviour
 {
     public GameObject circle;
 
